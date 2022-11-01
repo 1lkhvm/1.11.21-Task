e5e381647c1a6b17137e51177d25e42467ca7c91
1lkhvm <tu6wkkvyr@code.edu.az>
2022-11-01 23:48:31 +0400

    TaskComplete

---
+++ 1.11 tsk/cod.js
@@ -0,0 +1,123 @@
+
+//       var cars ={
+//         brand:Mercedes,
+//         model:C180,
+//         color:pink,
+//         Year:1998
+//     }
+
+//          var cars ={
+//         brand:Chevrolet,
+//         model:cruze,
+//         color:white,
+//         Year:2013
+
+//     }
+    
+//     var cars ={
+//         brand:Kia,
+//         model:Rio,
+//         color:black,
+//         Year:2009
+        
+//     }
+    
+//     var cars ={
+//         brand:Ford,
+//         model:Mustang,
+//         color:green,
+//         Year:2020
+        
+//     }
+    
+//     var cars ={
+//         brand:Chevrolet,
+//         model:Malibu,
+//         color:white,
+//         Year:2007
+        
+//     }
+
+    var machine 
+    [
+    
+        {
+            brand:"Mercedes",
+            model:"C180",
+            color:"pink",
+            Year:"1998",
+        },
+
+        {
+            brand:"Chevrolet",
+            model:"cruze",
+            color:"white",
+            Year:"2013",
+    
+        },
+        {
+            brand:"Kia",
+            model:"Rio",
+            color:"black",
+            Year:"2009",
+        },
+
+        {
+            brand:"Ford",
+            model:"Mustang",
+            color:"green",
+            Year:"2020",
+        },
+
+        {
+            brand:"Chevrolet",
+            model:"Malibu",
+            color:"white",
+            Year:"2007"
+            
+        },
+    ]
+    
+    var machine = 0;
+    var Enyenimasin =0;
+
+    for (i = 0; i < machine.length ; i++) {
+    
+       if(machine[i]>Enyenimasin)
+       Enyenimasin =machine[i]
+       
+    }
+ console.log(machine[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // {
+    //     var Ededler = [ "25", "23", "21", "24", "27", "65","42", "54", "6", "5", "31" ];
+    //     var ebe = Ededler[0];
+
+
+    //     for (int [i] = 0; i < Ededler.Length; i++)
+
+
+       
+    //     {
+    //         ifsss (Ededler[i] > ebe)
+    //             ebe = Ededler[i];
+  
+     
+    //     }
+    //     Console.WriteLine("En boyuk Eded EBE : {0}",ebe);
+    // }
---
+++ Cmsd/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        int[] Ededler = { 25, 23, 21, 24, 27, 65, 42, 2, 6, 5, 31 };
+//        int ebe = Ededler[0];
+
+
+//        for (int i = 0; i < Ededler.Length; i++)
+//        {
+//            if (Ededler[i] > ebe)
+//                ebe = Ededler[i];
+
+
+//        }
+//        Console.WriteLine("En boyuk Eded EBE : {0}", ebe);
+
+//    }
+
+
+//}
+
+
+
+//    int[] Ededler = { 5, 20, 28, 66, 78 };
+          
+//            int count = 0;
+//for (int i = 0; i < Ededler.Length; i++)
+
+//{
+//    if (Ededler[i] > 0 && Ededler[i] % 6 == 0)
+
+//    {
+//        Console.WriteLine(Ededler[i]);
+//        Ededler[i] += count;
+//    }
+//}
+
+//Console.WriteLine($"cemi" +count);
+
+
+
+
+
+
+
+
+
+
